38f8504fa34a4a59acb4764ff4671466ee0fff18
kylepalermo <kylepalermo@tamu.edu>
2025-04-25 14:42:15 -0500

    Added tetrahedron class

---
+++ src/Tetrahedron.cpp
@@ -0,0 +1,65 @@
+#include "Tetrahedron.h"
+
+Tetrahedron::Tetrahedron(const std::shared_ptr<Shape> shape) : 
+	tetrahedron(shape)
+{
+	x = { {
+		Eigen::Vector3d(0.0, 0.0, 0.0),
+		Eigen::Vector3d(1.0, 0.0, 0.0),
+		Eigen::Vector3d(0.0, 1.0, 0.0),
+		Eigen::Vector3d(0.0, 0.0, 1.0)
+	} };
+
+	faceIndices = { {
+		{{0, 1, 2}},
+		{{0, 2, 3}},
+		{{0, 3, 1}},
+		{{1, 3, 2}}
+	} };
+	faceOppositeIndices = { {
+		3, 1, 2, 0
+	} };
+}
+
+void Tetrahedron::draw(std::shared_ptr<MatrixStack> MV, const std::shared_ptr<Program> prog) const {
+	if (tetrahedron) {
+		MV->pushMatrix();
+		
+		glm::vec3 p0(x[0].x(), x[0].y(), x[0].z());
+		glm::vec3 p1(x[1].x(), x[1].y(), x[1].z());
+		glm::vec3 p2(x[2].x(), x[2].y(), x[2].z());
+		glm::vec3 p3(x[3].x(), x[3].y(), x[3].z());
+
+		glm::mat4 transform = glm::identity<glm::mat4>();
+		transform[0] = glm::vec4(p1 - p0, 0.0f);
+		transform[1] = glm::vec4(p2 - p0, 0.0f);
+		transform[2] = glm::vec4(p3 - p0, 0.0f);
+		transform[3] = glm::vec4(p0, 1.0f);
+		
+		MV->multMatrix(transform);
+		
+		glUniformMatrix4fv(prog->getUniform("MV"), 1, GL_FALSE, glm::value_ptr(MV->topMatrix()));
+		tetrahedron->draw(prog);
+		MV->popMatrix();
+	}
+}
+
+std::array<Face, 4> Tetrahedron::getFaces() const {
+	std::array<Face, 4> faces;
+	for (int i = 0; i < faces.size(); i++) {
+		Eigen::Vector3d p0 = x[faceIndices[i][0]];
+		Eigen::Vector3d p1 = x[faceIndices[i][1]];
+		Eigen::Vector3d p2 = x[faceIndices[i][2]];
+
+		Eigen::Vector3d n = (p1 - p0).cross(p2 - p0).normalized();
+		
+		if (n.dot(x[faceOppositeIndices[i]] - p0) > 0.0) {
+			n *= -1.0;
+		}
+		faces[i] = Face{
+			p0,
+			n
+		};
+	}
+	return faces;
+}
---
+++ src/Tetrahedron.h
@@ -0,0 +1,39 @@
+#ifndef TETRAHEDRON_H
+#define TETRAHEDRON_H
+
+#include <memory>
+
+#define EIGEN_DONT_ALIGN_STATICALLY
+#include <Eigen/Dense>
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include "Shape.h"
+#include "Program.h"
+#include "MatrixStack.h"
+
+struct Face {
+	Eigen::Vector3d x;
+	Eigen::Vector3d n;
+};
+
+
+class Tetrahedron {
+	const std::shared_ptr<Shape> tetrahedron;
+public:
+	Tetrahedron(const std::shared_ptr<Shape> shape);
+	void draw(std::shared_ptr<MatrixStack> MV, const std::shared_ptr<Program> prog) const;
+
+	std::array<Eigen::Vector3d, 4> x;
+	std::array<std::array<int, 3>, 4 > faceIndices;
+	std::array<int, 4> faceOppositeIndices;
+
+	std::array<Face, 4> getFaces() const;
+};
+
+#endif // !TETRAHEDRON_H
